a4c60e0ff4e976b9b4827603986e925e1363a06a
Patrick Weiss <patrick.weiss@officeone.team>
2019-09-20 13:28:34 +0200

    kommentare in d_promiseAll entfernt

--- d_promiseAll.js
+++ d_promiseAll.js
@@ -11,13 +11,6 @@ Promise.all([promise1, promise2])
   .then(values => {
     console.log("students and cities have been inserted");
     console.log(values);
-    /*
-    [ [ { _id: 5a4e462048841e65562c465a, firstName: 'Alice', __v: 0 },
-      { _id: 5a4e462048841e65562c465b, firstName: 'Bob', __v: 0 } ],
-    [ { _id: 5a4e462048841e65562c465c, name: 'Madrid', __v: 0 },
-      { _id: 5a4e462048841e65562c465d, name: 'Barcelona', __v: 0 },
-      { _id: 5a4e462048841e65562c465e, name: 'Paris', __v: 0 } ] ]
-    */
     mongoose.connection.close();
   })
   .catch(err => console.error(err));
